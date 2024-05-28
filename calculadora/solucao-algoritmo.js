52b3cab99dfb8ed209c13d44d727a6c86fa95ffe
MinCoffee98 <isa98vih@gmail.com>
2024-05-28 16:35:30 -0300

    Logica de Porgramação JS

---
+++ calculadora/solucao-algoritmo.js
@@ -0,0 +1,25 @@
+let notas = [];
+
+function adicionarNotas (){
+
+}
+
+function atualizarListaNotas (){
+
+}
+
+
+// 1. Criar uma calculadora
+// 2. Permitir  ler cada uma das notas {5,6,10}
+// 3. Calcular a média das notas = 5+6+10 = 21 * 3 = 7
+// 4. Se a média for > 6 = a pessoa estudante está aprovada
+// 5. Se a média for < = 6 a pessoa estudante está reprovada
+
+//funcionalidade = FUCTION calcularMedia
+//SE = IF - ELSE
+//LOOP = FOR = PARA
+
+function calcularMedia (){
+    let somatorioNotas = 0;
+    
+}
